af0e01c0915f92b58be092a37c5fa3ec2cb515e1
Hovhannes_Karapetian <hovhanneskarapetian1@gmail.com>
2020-07-28 13:20:24 +0400

    refactor

--- resolvers/BookResolver.ts
+++ resolvers/BookResolver.ts
@@ -2,9 +2,7 @@ import { BookQuery } from "../queries/BookQuery";
 import { BookMutation } from "../mutations/BookMutation";
 import { IResolvers } from "apollo-server";
 
-/**
- * @description holds book resolver
- */
+
 
 export const BookResolver: IResolvers = {
   Query: BookQuery,
